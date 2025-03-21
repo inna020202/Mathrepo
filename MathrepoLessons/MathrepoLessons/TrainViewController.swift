065c34114ed9665856e19e8dfab987aa75cf4ca3
GaiyInna <nina-belyaeva@list.ru>
2025-03-21 15:34:18 +0300

    add homework

--- MathrepoLessons/MathrepoLessons/TrainViewController.swift
+++ MathrepoLessons/MathrepoLessons/TrainViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 final class TrainViewController: UIViewController{
     
+    @IBOutlet var buttonCololections: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       configureButton()
+    }
+    private func  configureButton(){
+        //Add shadow
+        buttonCololections.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
+        }
+    }
+    
+    
+    
     //MARK: - Properties
     var type: MathTypes = .add{
         didSet {
