083c198145dd2458fd8634fa2eed426139fd9f6c
GaiyInna <nina-belyaeva@list.ru>
2025-04-03 00:42:23 +0300

    color button

--- MathrepoLessons/MathrepoLessons/TrainViewController.swift
+++ MathrepoLessons/MathrepoLessons/TrainViewController.swift
@@ -11,28 +11,101 @@ import UIKit
 final class TrainViewController: UIViewController{
     
     @IBOutlet var buttonCololections: [UIButton]!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       configureButton()
-    }
-    private func  configureButton(){
-        //Add shadow
-        buttonCololections.forEach { button in
-            button.layer.shadowColor = UIColor.darkGray.cgColor
-            button.layer.shadowOffset = CGSize(width: 0, height: 2)
-            button.layer.shadowOpacity = 0.4
-            button.layer.shadowRadius = 3
-        }
-    }
-    
-    
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
     
     //MARK: - Properties
+    private var firstNumber = 0
+    private var secondNumber = 0
+    private var sign: String = ""
+    private var count: Int = 0
+    
     var type: MathTypes = .add{
         didSet {
-            print(type)
+            switch type {
+            case .add:
+                sign = "+"
+            case .subtract:
+                sign  = "-"
+            case .multiply:
+                  sign = "*"
+            case .divide:
+                sign = "/"
+            }
         }
     }
-}
+    private var  answer: Int{
+        switch type {
+        case .add:
+            return firstNumber + secondNumber
+        case .subtract:
+            return firstNumber - secondNumber
+        case .multiply:
+            return firstNumber * secondNumber
+        case .divide:
+            return firstNumber / secondNumber
+        
+        }
+    }
+
+    
+    //MARK: - life cycle
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view.
+    configureQuestion()
+       configureButton()
+        }
+    
+    //MARK: - IBActions
+    @IBAction func leftAction(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    
+    @IBAction func rightAction(_ sender: UIButton) {
+        check(answer: sender.titleLabel?.text ?? "", for: sender)
+    }
+    
+    //MARK: - Methods
+    private func  configureButton(){
+        let buttonsArray = [leftButton,rightButton]
+        buttonsArray.forEach { button in
+            button?.backgroundColor = .yellow
+        //Add shadow
+            buttonsArray.forEach { button in
+            button?.layer.shadowColor = UIColor.darkGray.cgColor
+            button?.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button?.layer.shadowOpacity = 0.4
+            button?.layer.shadowRadius = 3
+        }
+        
+        let isRightButton = Bool.random()
+        var randomAnswer: Int
+        repeat {
+          randomAnswer = Int.random(in: (answer - 10)...(answer+10))
+    } while randomAnswer == answer
+        
+        rightButton.setTitle(isRightButton ? String(answer) : String(randomAnswer), for: .normal)
+        leftButton.setTitle(isRightButton ? String(randomAnswer) : String(answer), for: .normal)
+    }
+    }
+    
+   private func configureQuestion () {
+        firstNumber = Int.random (in: 1...99)
+        secondNumber = Int.random (in: 1...99)
+        
+        let question: String = "\(firstNumber) \(sign) \(secondNumber) = "
+        questionLabel.text  = question
+     }
+private func check (answer: String, for button: UIButton) {
+        let isRightAnswer = Int(answer) == self.answer
+        button.backgroundColor = isRightAnswer  ? .green : .red
+        if isRightAnswer {
+            count += 1
+            configureQuestion()
+            configureButton()
+                
+            }
+        }
+    }
+
